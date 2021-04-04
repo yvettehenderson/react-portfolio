f10bc55f938dcd9296329c96326c9d6941056a38
yvettehenderson <yvette.henderson@gmail.com>
2021-04-03 18:43:45 -0700

    added projects to home page

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,1 +1,81 @@
-import React from'react';
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+
+import cal from "../assets/cal.png";
+// import fitness from "../assets/";
+// import password from "../assets/";
+
+import Card from '../components/Card';
+
+
+class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        {
+          id: 0,
+          title: "Fitness Tracker",
+          subTitle: "",
+          // imgSrc: fitness,
+          link: " https://ancient-springs-30410.herokuapp.com/",
+          selected: false,
+        },
+        {
+          id: 1,
+          title: "Password Generator",
+          subTitle: "",
+          // imgSrc: password,
+          link: "https://yvettehenderson.github.io/password-generator/",
+          selected: false,
+        },
+        {
+          id: 2,
+          title: "Calendar",
+          subTitle: "",
+          imgSrc: cal,
+          link: "https://yvettehenderson.github.io/calendar/",
+          selected: false,
+        },
+      ],
+    };
+  }
+  handleCardClick = (id, card) => {
+
+    let items = [...this.state.items];
+
+    items[id].selected = items[id].selected ? false : true;
+
+    items.forEach(item => {
+        if(item.id !== id) {
+            item.selected = false;
+        }
+    });
+
+    this.setState({
+        items
+    });
+}
+
+makeItems = (items) => {
+    return items.map(item => {
+        return <Card item={item} click={(e => this.handleCardClick(item.id, e))} key={item.id} />
+    })
+}
+
+
+render() {
+    return(
+        <Container fluid={true}>
+            <Row className="justify-content-around">
+                {this.makeItems(this.state.items)}
+            </Row>
+        </Container>
+    );
+}
+
+}
+
+export default Carousel;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -9,6 +9,7 @@ function HomePage(props){
     return(
         <div>
             <Hero title={props.title} subTitle={props.subTitle} text={props.text} />
+            <Carousel/>
             
         </div>
     );
