3b3a0415ef04345520ba2d5001995ab7908e6e6e
yvettehenderson <yvette.henderson@gmail.com>
2021-04-07 13:11:18 -0700

    spelling error

--- src/pages/About.js
+++ src/pages/About.js
@@ -14,7 +14,7 @@ function AboutPage(props) {
             Throughout my 8 year agency career, I have contributed to impacting business outcomes through effective organization,prioritization, and execution of key projects. 
             In my Senior Project Manager roles, I exercised a calculated and methodical approach to problem
             solving. I am independently motivated, yet I appreciate team efforts and collaborate productively within
-            groups. Additionally, I am knowledgeable in Adobe, Microsoft Office, Google Docs, Slack, Basecamp,Smartsheets, Paymo, NetSuite and a Scrum Master.By Night, I am  a full stack web developer with experice in  Java Script,Express JS, Node JS, Oracle SQL, MongoDB, and React. </p>
+            groups. Additionally, I am knowledgeable in Adobe, Microsoft Office, Google Docs, Slack, Basecamp,Smartsheets, Paymo, NetSuite and a Scrum Master.By night, I am  a full stack web developer with experice in  Java Script,Express JS, Node JS, Oracle SQL, MongoDB, and React. </p>
 
             <p>You can find my LinkedIn profile<a className="git" href="https://www.linkedin.com/in/yvette-henderson/" target="_blank" rel="noopener noreferrer">here</a></p>
             <p>You can find my resume<a className="git" href="https://docs.google.com/document/d/1Vh3W_eedNPY-5DOkmfM1Pnr7un-swiUdA8Gl07MvTx0/edit?usp=sharing" target="_blank" rel="noopener noreferrer">here</a></p>
