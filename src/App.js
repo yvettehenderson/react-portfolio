dddb42b8f76746ad82df03af5834a3cf9fc4f991
yvettehenderson <yvette.henderson@gmail.com>
2021-04-08 18:17:18 -0700

    updated home

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ class App extends React.Component {
             <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
             <Navbar.Collapse id="navbar-toggle">
               <Nav className="ml-auto">
-                <Link className="nav-link" to="/react-portfolio">Home</Link>
+                <Link className="nav-link" to="/*">Home</Link>
                 <Link className="nav-link" to="/about">About</Link>
                 <Link className="nav-link" to="/contact">Contact</Link>
               </Nav>
